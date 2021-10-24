9568baf89df03c63900be9dbec1c841af5d226df
Anusid W <anusid.w@ku.th>
2021-10-25 00:32:22 +0700

    add method to change URL

--- test_registration_api.py
+++ test_registration_api.py
@@ -2,9 +2,24 @@
 import unittest
 import requests
 import requests_mock
-
+import re
 from unittest.mock import Mock
 
+
+
+def get_api(remove_param,replace):
+    URL = "https://wcg-apis.herokuapp.com/registration?citizen_id=1116789838901&name=Benjamin&surname=Lee&birth_date=1999-05-17&occupation=bartender&address=Bangkok"
+    if replace == "":
+        if re.search(f"{remove_param}(.*?)&", URL):
+            new_URL = re.sub(f"{remove_param}(.*?)&", "", URL)
+        else:
+            new_URL = re.sub(f"{remove_param}(.*?)", "", URL)
+    else:
+        if re.search(f"{remove_param}(.*?)&", URL):
+            new_URL = re.sub(f"{remove_param}(.*?)&", f"{remove_param}={replace}&", URL)
+        else:
+            new_URL = re.sub(f"{remove_param}(.*?)", f"{remove_param}={replace}", URL)
+    return new_URL
 
 def post_registration():
     """Posting registration for mock"""
@@ -42,14 +57,12 @@ class RegistrationAPITest(unittest.TestCase):
 
     def test_post_registration_missing_citizen_id(self):
         """Test registered with missing citizen_id as query param."""
-        self.response = requests.post(
-            "https://wcg-apis.herokuapp.com/registration?name=Benjamin&surname=Lee&birth_date=1999-05-17&occupation=bartender&address=Bangkok")
+        self.response = requests.post(get_api(remove_param="citizen_id",replace=""))
         self.assertEqual(self.response.status_code, 400)
 
     def test_post_registration_non_13_digit_citizen_id(self):
         """Test registered with citizen_id that isn't 13 digits."""
-        self.response = requests.post(
-            "https://wcg-apis.herokuapp.com/registration?citizen_id=1111&name=Benjamin&surname=Lee&birth_date=1999-05-17&occupation=bartender&address=Bangkok")
+        self.response = requests.post(get_api(remove_param="citizen_id", replace=1111))
         self.assertEqual(self.response.status_code, 200)
         self.assertEqual({
             "feedback": "registration failed: invalid citizen ID"
@@ -57,36 +70,38 @@ class RegistrationAPITest(unittest.TestCase):
 
     def test_post_registration_missing_name(self):
         """Test registered with missing name as query param."""
-        self.response = requests.post(
-            "https://wcg-apis.herokuapp.com/registration?citizen_id=1116789838901&surname=Lee&birth_date=1999-05-17&occupation=bartender&address=Bangkok")
+        self.response = requests.post(get_api(remove_param="name", replace=""))
         self.assertEqual(self.response.status_code, 400)
+        
 
     def test_post_registration_using_integer_name(self):
         """Test registered using name that is integer."""
-        self.response = requests.post(
-            "https://wcg-apis.herokuapp.com/registration?citizen_id=1116789838901&name=123&surname=Lee&birth_date=1999-05-17&occupation=bartender&address=Bangkok")
+        self.response = requests.post(get_api(remove_param="name", replace="123"))
+        with self.assertRaises(TypeError):
+            self.response
         self.assertEqual(self.response.status_code, 400)
+        
 
     def test_post_registration_missing_surname(self):
         """Test registered with missing surname as query param."""
-        self.response = requests.post(
-            "https://wcg-apis.herokuapp.com/registration?citizen_id=1116789838901&name=Benjamin&birth_date=1999-05-17&occupation=bartender&address=Bangkok")
+        self.response = requests.post(get_api(remove_param="surname", replace=""))
         self.assertEqual(self.response.status_code, 400)
 
     def test_post_registration_using_integer_surname(self):
         """Test registered using surname that is integer."""
-        self.response = requests.post(
-            "https://wcg-apis.herokuapp.com/registration?citizen_id=1116789838901&name=Benjamin&surname=123&birth_date=1999-05-17&occupation=bartender&address=Bangkok")
+        self.response = requests.post(get_api(remove_param="surname", replace="123"))
+        with self.assertRaises(TypeError):
+            self.response
         self.assertEqual(self.response.status_code, 400)
 
     def test_post_registration_missing_birth_date(self):
         """Test registered with missing birth_date as query param."""
-        self.response = requests.post(
-            "https://wcg-apis.herokuapp.com/registration?citizen_id=1116789838901&name=Benjamin&surname=Lee&occupation=bartender&address=Bangkok")
+        self.response = requests.post(get_api(remove_param="birth_date", replace=""))
         self.assertEqual(self.response.status_code, 400)
 
     def test_post_registration_incorrect_birth_date_format(self):
         """Test registered using %Y-%d-% format for birth_date."""
+        # this have to be full URL to use different person
         self.response = requests.post(
             "https://wcg-apis.herokuapp.com/registration?citizen_id=1116789838110&name=Benedict&surname=Tan&birth_date=1999-17-05&occupation=bartender&address=Bangkok")
         self.assertEqual(self.response.status_code, 200)
@@ -96,6 +111,7 @@ class RegistrationAPITest(unittest.TestCase):
 
     def test_post_registration_less_than_12_years_old(self):
         """Test registered with age less than 12 years old."""
+        # this have to be full URL to use different person
         self.response = requests.post(
             "https://wcg-apis.herokuapp.com/registration?citizen_id=1116789838110&name=Benedict&surname=Tan&birth_date=2018-05-17&occupation=bartender&address=Bangkok")
         self.assertEqual(self.response.status_code, 200)
@@ -105,14 +121,12 @@ class RegistrationAPITest(unittest.TestCase):
 
     def test_post_registration_missing_occupation(self):
         """Test registered with missing occupation as query param."""
-        self.response = requests.post(
-            "https://wcg-apis.herokuapp.com/registration?citizen_id=1116789838901&name=Benjamin&surname=Lee&birth_date=1999-05-17&address=Bangkok")
+        self.response = requests.post(get_api(remove_param="occupation", replace=""))
         self.assertEqual(self.response.status_code, 400)
 
     def test_post_registration_missing_address(self):
         """Test registered with missing address as query param."""
-        self.response = requests.post(
-            "https://wcg-apis.herokuapp.com/registration?citizen_id=1116789838901&name=Benjamin&surname=Lee&birth_date=1999-05-17&occupation=bartender")
+        self.response = requests.post(get_api(remove_param="address", replace=""))
         self.assertEqual(self.response.status_code, 400)
 
 
